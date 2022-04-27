12389f60c54dce50a450afdb5002210bb3b13507
nggill <92691516+nggill@users.noreply.github.com>
2022-04-26 19:17:09 -0500

    began structure for javascript

---
+++ countdown-timer/script.js
@@ -0,0 +1,8 @@
+const newYears='1st Jan 2023'
+function countdown(){
+    const newYearsDate= new Date(newYears);
+    const currentDate= newDate();
+
+    console.log(newYearsDate-currentDate);
+}
+countdown(); 
