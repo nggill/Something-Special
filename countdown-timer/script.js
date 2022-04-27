d5cc17a9215f24f9b3c5c266f7823dee018c1d4e
nggill <92691516+nggill@users.noreply.github.com>
2022-04-27 17:52:48 -0500

    corrected JS for countdown app

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,9 +1,20 @@
-const newYears="1st Jan 2023";
+const newYears="1 Jan 2023";
 
 function countdown(){
     const newYearsDate= new Date(newYears);
     const currentDate= new Date();
 
-    console.log(newYearsDate-currentDate);
+    const totalSeconds = (newYearsDate-currentDate) / 1000;
+
+    const days= Math.floor(totalSeconds/3600/24);
+    const hours= Math.floor(totalSeconds/3600) % 24;
+    const minutes =(Math.floor(totalSeconds/60)% 24) %60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+
+    console.log(days);
 }
+///initial call
 countdown(); 
+
+setInterval(countdown, 1000);
